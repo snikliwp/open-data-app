cd075624fc3adc8470e9bbf34f455f37cb5bbf74
snikliwp <wilk0146@algonquinlive.com>
2012-03-05 14:59:05 -0500

    first cut

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 	require_once 'includes/filter-wrapper.php';
 require_once 'includes/db.php';
 
-$results = $db->query('SELECT id, title, release_date, director 
-				FROM movies 
-				ORDER BY release_date ASC');
+$results = $db->query('SELECT id, name, xcoord, ycoord 
+				FROM openDataApp 
+				ORDER BY name ASC');
 ?>
 
 
@@ -12,31 +12,31 @@ $results = $db->query('SELECT id, title, release_date, director
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Movies</title>
-	<link href="css/general.css" rel="stylesheet">
+	<title>Museums</title>
+	<link href="css/public.css" rel="stylesheet">
 </head>
 
 <body>
 		<table border="1">
 <!--	The caption element briefly describes the contents of the table -->
-		<caption>Movie List</caption>
+		<caption>Museums</caption>
 	<!--	Header Rows of the table describe each of the columns -->
 	<!-- colgroup element allows us to semantically group columns and apply css to a column -->
 			<tbody>
-		<?php foreach ($results as $movie) : ?>
+		<?php foreach ($results as $museum) : ?>
 		 <tr><td>
-		 <a href="single.php?id=<?php echo $movie['id'];?>"><?php  echo $movie['title']; ?> </a>
+		 <a href="single.php?id=<?php echo $museum['id'];?>"><?php  echo $museum['name']; ?> </a>
 		 </td><td>
-		<a href="edit.php?id=<?php echo $movie['id'];?>">Edit</a>
+		<a href="edit.php?id=<?php echo $museum['id'];?>">Edit</a>
 		 </td><td>
-		<a href="delete.php?id=<?php echo $movie['id'];?>">Delete</a>
+		<a href="delete.php?id=<?php echo $museum['id'];?>">Delete</a>
 		</td>
 		 <?php endforeach ?>
 </nav>
 			</tbody>
 		</table>
 
-	<a href="add.php"><button class="add">Add a New Movie</button></a>
+	<a href="add.php"><button class="add">Add a New Museum</button></a>
 
 </body>
 </html>
