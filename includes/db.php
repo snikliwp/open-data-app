b07348b9943f17edc954996477473164cecd878c
snikliwp <wilk0146@algonquinlive.com>
2012-03-05 12:49:00 -0500

    changed to stripslahes

--- includes/db.php
+++ includes/db.php
@@ -3,8 +3,8 @@
 // this file takes the environmental values created in .htaccess file and sets them into php
 
 $user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$dsn = getenv('DB_DSN');
+$pass = getenv('DB_PASSWORD');
+$dsn = stripslashes(getenv('DB_DSN'));
 
 $db = new PDO($dsn, $user, $pass);
 $db->exec('SET NAMES utf8');
